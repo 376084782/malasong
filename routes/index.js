3f9112bf65305108cabf7e8ba4f43d47256e4f7a
钱丽云 <376084782@qq.com>
2021-03-18 21:28:50 +0800

    增加名片识别的接口

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,45 @@ import ModelUser1 from '../models/ModelUser1'
 var router = express.Router();
 
 
+var request = require('request');
+var FormStream = require('formstream')
+
+
 /* GET home page. */
+
+router.post('/scan', async (req, res, next) => {
+  let data = req.body;
+  request.post({
+    url: 'http://jisumpsb.market.alicloudapi.com/businesscardrecognition/recognize',
+    headers: {
+      Authorization: "APPCODE " + '82a9901359d24e01ac548b44c21fd41b',
+    },
+    form: {
+      pic: data.pic
+    }
+  }, function (error, response, body) {
+    // console.log(response, body, 'data')
+    if (!error && response.statusCode == 200) {
+      console.log(body, '请求成功的处理逻辑') // 请求成功的处理逻辑
+      res.json({
+        code: 0,
+        data: body
+      });
+    } else {
+      console.log(error, response, body,'error')
+      res.json({
+        code: -1,
+      });
+    }
+  }, (e) => {
+    console.log(e, 'complete')
+
+    res.json({
+      code: -1,
+    });
+  });
+});
+
 router.post('/doSubmit', async (req, res, next) => {
   let data = req.body;
   let dataDB = await ModelUser1.findOne({
@@ -46,7 +84,7 @@ router.post('/doSubmit', async (req, res, next) => {
 router.post('/getData', async (req, res, next) => {
   let data = req.body;
   let dataDB = await ModelUser1.findOne({
-    phone: '' + data.phone,
+    phone: data.phone,
     name: data.name
   })
   if (dataDB) {
@@ -85,45 +123,45 @@ router.post('/login', async (req, res, next) => {
     });
   }
 });
-router.post('/scan', async (req, res, next) => {
-  let data = req.body;
-  let timeNow = new Date().getTime();
-  if (timeNow - data.time > 60 * 1000 * 5) {
-    res.json({
-      code: -1,
-      message: '验证码已过期，请刷新重试'
-    });
-    return
-  }
-  let dataDB = await ModelUser1.findOne({
-    cardId: '' + data.cardId
-  })
-  if (dataDB) {
-    let listGamePlayed = dataDB.listGamePlayed.split(',');
-    if (listGamePlayed.indexOf(data.gameId) > -1) {
-      res.json({
-        code: -1,
-        message: '您已经打过卡了'
-      });
-    } else {
-      listGamePlayed.push(data.gameId);
-      await dataDB.updateOne({
-        listGamePlayed: listGamePlayed.join(',')
-      })
-      res.json({
-        code: 0,
-        data: {
+// router.post('/scan', async (req, res, next) => {
+//   let data = req.body;
+//   let timeNow = new Date().getTime();
+//   if (timeNow - data.time > 60 * 1000 * 5) {
+//     res.json({
+//       code: -1,
+//       message: '验证码已过期，请刷新重试'
+//     });
+//     return
+//   }
+//   let dataDB = await ModelUser1.findOne({
+//     cardId: '' + data.cardId
+//   })
+//   if (dataDB) {
+//     let listGamePlayed = dataDB.listGamePlayed.split(',');
+//     if (listGamePlayed.indexOf(data.gameId) > -1) {
+//       res.json({
+//         code: -1,
+//         message: '您已经打过卡了'
+//       });
+//     } else {
+//       listGamePlayed.push(data.gameId);
+//       await dataDB.updateOne({
+//         listGamePlayed: listGamePlayed.join(',')
+//       })
+//       res.json({
+//         code: 0,
+//         data: {
 
-        }
-      });
-    }
-  } else {
-    res.json({
-      code: 9999,
-      message: '您信息填写有误或未报名此次活动2'
-    });
-  }
-});
+//         }
+//       });
+//     }
+//   } else {
+//     res.json({
+//       code: 9999,
+//       message: '您信息填写有误或未报名此次活动2'
+//     });
+//   }
+// });
 router.post('/played', async (req, res, next) => {
   let data = req.body;
   let dataDB = await ModelUser1.findOne({
